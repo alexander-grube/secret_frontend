d5813c9a96e5427750b122eb555c4398f156ba4e
Alexander Grube <alexander.grube@cancom.de>
2022-04-12 21:29:32 +0200

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+
+import './index.css'
+import App from './App'
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root') as Element;
+const root = createRoot(container);
+root.render(<App />);
