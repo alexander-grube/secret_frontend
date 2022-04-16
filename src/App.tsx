301bc6a6838a2c851cc15d4fd92ac458843c17e8
Alexander Grube <alexp.grube@googlemail.com>
2022-04-16 11:12:30 +0200

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,44 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+  const [isActiveHour, setActiveHour] = useState(false);
+  const [isActiveDay, setActiveDay] = useState(false);
+  const [isActiveWeek, setActiveWeek] = useState(false);
+
+  const toggleHour = () => {
+    setActiveHour(!isActiveHour);
+    setActiveDay(false);
+    setActiveWeek(false);
+  };
+
+  const toggleDay = () => {
+    setActiveDay(!isActiveDay);
+    setActiveHour(false);
+    setActiveWeek(false);
+  };
+
+  const toggleWeek = () => {
+    setActiveWeek(!isActiveWeek);
+    setActiveHour(false);
+    setActiveDay(false);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <h2>Secret Message</h2>
+        <p>Lifetime</p>
+        <div className="btn-group">
+          <button onClick={toggleHour} className={isActiveHour ? 'active': undefined}>1 Hour</button>
+          <button onClick={toggleDay} className={isActiveDay ? 'active': undefined}>1 Day</button>
+          <button onClick={toggleWeek} className={isActiveWeek ? 'active': undefined}>1 Week</button>
+        </div>
+        <textarea className='textarea-responsive' placeholder='Secret message goes here...' value={message} onChange={(e) => setMessage(e.target.value)} />
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button type="button" onClick={() => console.log(message)}>
+            Create Secret
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
@@ -26,15 +50,6 @@ function App() {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
           </a>
         </p>
       </header>
